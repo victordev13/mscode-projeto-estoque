332de6e1dd2a4d31e1bf36a876dea0fd4f50e2f0
victordev13 <vtrcarvalho.13@gmail.com>
2024-08-26 19:57:43 -0300

    feat: login

--- public/routes.php
+++ public/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controller\AppController;
 use App\Controller\AutenticarController;
 use App\Controller\Error\NotFoundController;
 use App\Controller\LoginController;
@@ -8,7 +9,8 @@ $router = [
     'routes' => [
         '/' => LoginController::class,
         '/login' => LoginController::class,
-        '/login/autenticar' => AutenticarController::class
+        '/login/autenticar' => AutenticarController::class,
+        '/app' => AppController::class
     ],
     'default' => NotFoundController::class
 ];
