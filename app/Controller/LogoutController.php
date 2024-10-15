c604882ce2f7f6c5a72bac2d0c41270406b3afc0
Victor Carvalho <vtrcarvalho.13@gmail.com>
2024-10-15 18:50:51 -0300

    Update LogoutController.php

--- app/Controller/LogoutController.php
+++ app/Controller/LogoutController.php
@@ -6,7 +6,7 @@ class LogoutController extends AbstractController
 {
     public function index(array $data): void
     {
-       unset($_SESSION['NOME']);
+        session_destroy();
 
         $this->redirect('/login');
     }
